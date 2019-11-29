373f0c52418fc63d61e88b83ca1d65c761f77e32
haz826 <57448324+haz826@users.noreply.github.com>
2019-11-29 17:58:40 +0300

    Delete dbo.Table.sql

--- EComProject/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[ProductID] INT NOT NULL PRIMARY KEY, 
-    [ProductName] VARCHAR(50) NOT NULL, 
-    [Price] BIGINT NOT NULL, 
-    [ProductImg] VARCHAR(50) NOT NULL
-)
